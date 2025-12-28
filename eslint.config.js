b53540891c9dca57e919aa2e5ae80b12061addc2
Aleksandr Mikhaylenko <aleksandr.mikhaylenko@fiverr.com>
2025-12-28 22:32:15 +0100

    chore: update project configuration

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+import { default as defaultConfig } from '@epic-web/config/eslint';
+
+/** @type {import("eslint").Linter.Config[]} */
+export default [
+	...defaultConfig,
+	{
+		ignores: ['.react-router/**'],
+	},
+];
---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+import defaultConfig from '@epic-web/config/prettier';
+
+/** @type {import("prettier").Options} */
+export default {
+	// semi: true,
+	...defaultConfig,
+	semi: true,
+};
